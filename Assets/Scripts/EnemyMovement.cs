5a2bc2dbd820c4040f3ca14dfb5ae71513f1e3bd
Jacob Kilgore <jake.h.kilgore@gmail.com>
2020-04-18 18:29:14 -0500

    feature: Added basic enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField][Range(-5, 5)]
+    private float moveSpeed;
+    [SerializeField][Range(0, 1)]
+    private float amplitude = 0.5f;
+    [SerializeField][Range(1, 10)]
+    private float frequency = 20f;
+
+    private void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.MovePosition(new Vector2(body.position.x + moveSpeed * Time.fixedDeltaTime, Mathf.Sin(2 * Mathf.PI * frequency * Time.time) * amplitude));
+    }
+}
